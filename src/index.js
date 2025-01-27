1a42156bf93b0dfebd41e89b4b783e6533cabd7d
Saicharankappala <kappalasaicharan22@gmail.com>
2025-01-27 00:35:39 -0800

    Fix deployment issues for Vercel

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-
 const originalError = console.error;
 console.error = (...args) => {
   if (args[0] && args[0].includes("ResizeObserver loop")) {
@@ -11,7 +10,9 @@ console.error = (...args) => {
   originalError(...args);
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
-<React.StrictMode>
-  <App />
-</React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
